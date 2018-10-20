10549838c02043fe7969ac2c16a3204c3bd7b1c1
ehsanaziz91 <ehsanaziz96@gmail.com>
2018-10-20 16:55:36 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,33 +2,30 @@
 session_start();
 include "connection.php";
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$id = $_POST['staffid'];
+$pass = $_POST['password'];
 
-$sql = "SELECT user_id, username, name, password FROM users WHERE username='$username' and password='$password'";
+$sql = "SELECT staffID, password FROM staff WHERE staffID='$id' and password='$pass'";
 
 $execute = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 
-if(mysqli_num_rows($execute)>0){
+if(mysqli_num_rows($execute)>0)
+{
 	//otput data of each row
-	while($row = mysqli_fetch_assoc($execute)){
-		
-		
+	while($row = mysqli_fetch_assoc($execute))
+    {
 		//set the session
-		$_SESSION['username'] = $row["username"];
-		$_SESSION['name'] = $row["name"];
-		$_SESSION['id'] = $row["user_id"];
+		$_SESSION['staffid'] = $row["staffID"];
 		
 		echo "<script>alert('Login Success!');</script>";
 		echo "<meta http-equiv='refresh' content='0; url= main.php'/>";
 		
 	}
-}else{
+}else
+{
 	echo "<script>alert('Login fail!');</script>";
 	echo "<meta http-equiv='refresh' content='0; url= index.php'/>";
 }
 mysqli_close($conn);
-
-
 
 ?>
