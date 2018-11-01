8f3b5e02e3ea8ddfc795b16e67d9c1e5bc6f8709
hazirahshabri <44309082+hazirahshabri@users.noreply.github.com>
2018-11-01 14:19:39 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,18 +4,18 @@ include "connection.php";
 
 $id = $_POST['staffid'];
 $pass = $_POST['password'];
-
-$sql = "SELECT staffID, password FROM staff WHERE staffID='$id' and password='$pass'";
-
-$execute = mysqli_query($conn, $sql) or die (mysqli_error($conn));
+$sql = "SELECT staffID,staffname, password FROM staff WHERE staffID='$id' and password='$pass'";
+$execute = mysqli_query($conn, $sql) or die (mysqli_error($conn)); //connection dengan database
 
 if(mysqli_num_rows($execute)>0)
 {
 	//otput data of each row
-	while($row = mysqli_fetch_assoc($execute))
+	while($row = mysqli_fetch_assoc($execute)) //check SQL return result tak
     {
 		//set the session
 		$_SESSION['staffid'] = $row["staffID"];
+		$_SESSION['name'] = $row["staffname"];
+		
 		
 		echo "<script>alert('Login Success!');</script>";
 		echo "<meta http-equiv='refresh' content='0; url= main.php'/>";
@@ -29,3 +29,5 @@ if(mysqli_num_rows($execute)>0)
 mysqli_close($conn);
 
 ?>
+
+
