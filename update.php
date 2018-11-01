87aa1e0723754d86ea2a7b91f1dc74897c0263ef
hazirahshabri <44309082+hazirahshabri@users.noreply.github.com>
2018-11-01 14:18:16 +0800

    Update update.php

--- update.php
+++ update.php
@@ -2,19 +2,29 @@
 session_start();
 include "connection.php";
 
-$bookId = $_GET['bookId'];
-$sql = " SELECT book_id, book_name, quantity, price FROM book WHERE book_id = '$bookId' ";
-$result = mysqli_query ($conn, $sql) or die (mysqli_error($conn));
+$matricNo = $_GET['studentId'];
+/*
+$studName = $_POST["name"];  --> //Yang belah kiri amik value belah kanan 
+$matricNo = $_POST["studentId"]; //based on html kita
+$phone = $_POST["phone"];
+$address = $_POST["address"];
+$faculty = $_POST["faculty"];
+$gender = $_POST["gender"];
+*/
+
+$sql ="SELECT studentName, studentID, phoneNo, address, faculty, gender from student WHERE studentID = '$matricNo' " ;  // SQL based on database declaration //letk condition  !!
+$result = mysqli_query($conn,$sql) or die (mysqli_error($conn));
 $data = mysqli_fetch_assoc($result);
-//echo $data['book_id']
 
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Store System</title>
-    <link rel="icon" href="Logo/bag.jpg" type="image/gif" sizes="16x16">
+    <link rel="icon" href="Logo/5.jpg" type="image/gif" sizes="16x16">
     <!-- external css -->
     <link rel="stylesheet" href="style.css" type="text/css">
     <!-- internal css -->
@@ -35,38 +45,85 @@ $data = mysqli_fetch_assoc($result);
           </div>
           <!-- box of content -->
           <div class="Mcontent">
-            <br><br><br><br>
+		  
             <table align="center" border="0" width="50%" cellpadding="0" cellspacing="0">
               <td><hr></td>
-              <td width="30%" align="center"><h2><font color="#1168da">Update Books</font></h2></td>
+              <td width="40%" align="center"><h2><p><img src="logo/updates.jpg" alt="icon update" width="42" height="42" align="center" hspace="20"><font color="#8B008B">Update Student</font></h2></td>
               <td><hr></td>
             </table>
             <!-- Register Books Content -->
             <form action="update_action.php" method="post">
               <table align="center" border="0" width="40%" cellpadding="5" cellspacing="0">
+			
                 <tr>
-                  <td align="center" width="30%">Book Name</td>
-                  <td><input type="hidden" name="bookId" value="<?php echo $data["book_id"];?>"><input class="Minput" type="text" name="name" value="<?php echo $data["book_name"];?>" autofocus required></td>
+                  <td align="center" width="30%">Student Name</td>
+                  <td><input class="Minput2" type="text" name="name" value="<?php echo $data["studentName"];?>" autofocus required></td>
+                <!-- echo $data["database declaration"]; -->
+				<!-- name="" kena sama dengan post yg kt atas -->
+				</tr>
+                <tr>
+                  <td align="center" width="30%">Matric No</td>
+                  <td><input class="Minput2" type="text" name="studentId" value="<?php echo $data["studentID"];?>"></td>
                 </tr>
                 <tr>
+                  <td align="center" width="30%">Phone No</td>
+                  <td><input class="Minput2" type="number" name="phone" value="0<?php echo $data["phoneNo"];?>"required></td>
+                </tr>
+				<tr>
+                  <td align="center" width="30%">Current Address</td>
+                  <td><input class="Minput2" type="text" name="address" value="<?php echo $data["address"];?>"required></td>
+                </tr>
+				<tr>
+                  <td align="center" width="30%">Faculty</td>
+                  <td><input class="Minput2" type="text" name="faculty" value="<?php echo $data["faculty"];?>"required></td>
+                </tr>
+				
+				
+				
+				<!--
+				<tr>
+                  <td align="center" width="30%">Sem</td>
+                  <td><input type="checkbox" name="number" value = "sem 1" required>Sem 1</td>
+				  <td><input type="checkbox" name="number" value = "sem 2" required>Sem 2</td>
+                </tr>
+				<tr>
+                  <td align="center" width="30%">Sessions</td>
+                  <td><input class="Minput" type="number" name="number" required></td>
+                </tr>
+				<tr>
+                  <td align="center" width="30%">Date Received</td>
+                  <td><input class="Minput" type="date" name="date received" required></td>
+                </tr>
+				<tr>
+                  <td align="center" width="30%">Date Received</td>
+                  <td><input class="Minput" type="date" name="date received" required></td>
+                </tr>
+				<tr>
+                  <td align="center" width="30%">Description</td>
+                  <td><input class="Minput" type="text" name="descriptioninput" required></td>
+                </tr>
+				<tr>
+                  <td align="center" width="30%">Type</td>
+                  <td><input class="Minput" type="text" name="typeinput" required></td>
+                </tr>
+				<tr>
                   <td align="center" width="30%">Quantity</td>
-                  <td><input class="Minput" type="number" name="quantity" value="<?php echo $data["quantity"];?>"required></td>
+                  <td><input class="Minput" type="number" name="quantityinput" required></td>
                 </tr>
-                <tr>
-                  <td align="center" width="30%">Price</td>
-                  <td><input class="Minput" type="number" name="price" value="<?php echo $data["price"];?>" name="price" step="0.01" required></td>
-                </tr>
+				-->
+				<td></td>
                 <tr>
                   <td></td>
-                  <td><input class="Lbtn" type="submit" name="submit" value="submit"></td>
+                  <td><input class="Lbtn2" type="submit" name="submit" value="submit"></td>
                 </tr>
               </table>
             </form>
+			
           </div>
           <!-- box of footer -->
           <div class="footer">
       				&#9400; UTeM STUDENT CENTRE STORE MANAGEMENT SYSTEM <br>
-					Time : 8.00 a.m - 4.00 p.m <br>
+					Time : 10.00 a.m - 4.00 p.m <br>
 					Place : Level 2,UTeM Student Centre 
       		</div>
       </div>
