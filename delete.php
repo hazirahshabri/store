b8664951159744e2ff54f379ef6eef5cd54ad7c3
hazirahshabri <44309082+hazirahshabri@users.noreply.github.com>
2018-11-01 13:36:45 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,9 +2,9 @@
 <?php
 session_start();
 include "connection.php";
-$bookId = $_GET['bookId'];
-$sql = "DELETE FROM book WHERE book_id = '$bookId'";
+$matricNo = $_GET["studentId"];
+$sql = "DELETE FROM student WHERE studentID = '$matricNo'";
 $execute = mysqli_query($conn, $sql) or die (mysqli_error($conn));
 echo "<script>alert('Delete Success!');</script>";
-		echo "<meta http-equiv='refresh' content='0; url= action.php'/>";
+		echo "<meta http-equiv='refresh' content='0; url= list.php'/>";
 ?>
