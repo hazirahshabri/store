1b49c6b2c7395b330d0f764f385c5bb4adf59c30
hazirahshabri <44309082+hazirahshabri@users.noreply.github.com>
2018-11-01 14:23:11 +0800

    Update menu.php

---
+++ menu.php
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html>
+<table border="0" width="100%" cellpadding="0" cellspacing="0">
+  <tr>
+    <tr width="100%"><h2><font color="#8B008B"> UTeM STUDENT CENTRE STORE MANAGEMENT SYSTEM</font></h2></tr>
+	<br>
+	<tr></tr>
+	<tr>
+	<td width="0%" align="center"><a href="main.php" class="menu">menu</a>
+    <td width="0%" align="center">
+	<div class = "dropdown">
+		<a href="insert_student.php" class="dropbtn">Manage Student</a>
+			<div class="dropdown-content">
+				<a href="insert_student.php" class="menu">Insert</a>
+				<!-- <a href="update.php" class="menu">Update</a> -->
+				<a href="list.php" class="menu">List</a>
+			</div>
+	</div>
+	<td width="0%" align="center"><a href="item.php" class="menu">student items</a></td>
+	<td width="0%" align="center"><a href="display.php" class="menu">room & locker</a></td>
+	<td width="0%" align="center"><a href="payment.php" class="menu">payment</a></td>
+	<td width="0%" align="center"><a href="report.php" class="menu">report</a></td>
+    <td width="0%" align="center"><a href="logout.php" class="menu" onClick="return confirm('Are You Sure?')">Logout</a></td>
+	</tr>
+</table>
+
+
+
+
