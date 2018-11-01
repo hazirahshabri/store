c95d84c79d1c7938c7d9f87d502e052058f978c1
hazirahshabri <44309082+hazirahshabri@users.noreply.github.com>
2018-11-01 14:16:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Store System</title>
-	<link rel="icon" href="Logo/bag.jpg" type="image/gif" sizes="16x16">
+	<link rel="icon" href="Logo/5.jpg" type="image/gif" sizes="16x16">
     <!-- external css -->
     <link rel="stylesheet" href="style.css" type="text/css">
     <!-- internal css -->
@@ -28,13 +28,26 @@
             </table>
           </div>
           <!-- box of content -->
-          <div class="Mcontent">
-		   
-            <disc>
-              <table align="center" border="0" width="90%" cellpadding="7" cellspacing="0">
-                <tr>
-				<td width="10%"><img src="Logo/UTeM.jpg" width="350px" height="300px"></td>
-                  <td width="30%">
+          <div class="Mcontent2">
+              <table align="center" border="0" width="80%" cellpadding="7" cellspacing="0">
+                
+				<td><form action="login.php" method="post">
+                  <td width="35%" align="center"><h2><font color="#8B008B">LOGIN</font></h2>
+                    Staff ID <input class="input" type="text" name="staffid" autofocus required><br><br>
+                    Password &nbsp;<input class="input" type="password" name="password" required><br><br>
+					
+                    <input class="Lbtn" type="submit" name="login" value="LOGIN"><br><br>
+					<input class="Lbtn" type="reset" name="reset" value="RESET"><br><br>
+					
+                    Don't have an account? <a href="register.php"> Sign Up </a>
+                  </td>
+                    </form>
+				  
+				 <td style="border-right: 1.5px solid #DDA0DD;"> <!-- inline css --></td>
+				 <td></td>
+				 <td></td>
+				<td width="30%"><img src="Logo/UTeM.jpg" width="320px" height="290px"></td>
+                  <td width="40%">
                     <h2><font color="#8B008B">ABOUT US</font></h2>
                     <p> Welcome to our service! <br><br>
                        You now can register as a user to join and using our system. <br><br>
@@ -43,26 +56,15 @@
 					   Thank You !
                     </p>
                   </td>
-                  <td style="border-right: 1px solid #DDA0DD;"> <!-- inline css --></td>
                   <td></td>
-				  
-                  <form action="login.php" method="post">
-                  <td width="35%" ><h2><font color="#8B008B">LOGIN</font></h2>
-                    User ID <input class="input" type="text" name="staffid" autofocus required><br><br>
-                    Password &nbsp;<input class="input" type="password" name="password" required><br><br>
-                    <input class="Lbtn" type="submit" name="login" value="LOGIN"><br><br>
-					
-                    Don't have an account? <a href="register.php"> Sign Up </a>
-                  </td>
-                  </form>
                 </tr>
               </table>
-            </disc>
+            
           </div>
 		  <!-- box of footer -->
           <div class="footer">
       				&#9400; UTeM STUDENT CENTRE STORE MANAGEMENT SYSTEM <br>
-					Time : 8.00 a.m - 4.00 p.m <br>
+					Time : 10.00 a.m - 4.00 p.m <br>
 					Place : Level 2,UTeM Student Centre 
       			
       </div>
